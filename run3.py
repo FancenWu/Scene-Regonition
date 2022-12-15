7517fc0b5c608d5086e9b547b398afd2accc6856
Chenfan Wu <cw1a20@soton.ac.uk>
2022-12-15 21:52:12 +0000

    Get familiar with SIFT and it's keypoints

--- run3.py
+++ run3.py
@@ -1,9 +1,17 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import glob
 
 
 class_paths = glob.glob('dataset/training/*')
 class_names = [name[17:] for name in class_paths]
-print(class_paths)
-print(class_names)
+
+print(cv.__version__)
+img1 = cv.imread('dataset/testing/0.jpg')
+gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+sift = cv.SIFT_create()
+kp = sift.detect(gray, None)
+img = cv.drawKeypoints(gray, kp, img1)
+cv.imwrite('sift_keypoint.jpg', img)
+
+
