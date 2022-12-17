1a786696d33798ed3b0f68bc5968d0073be5fcc4
Chenfan Wu <cw1a20@soton.ac.uk>
2022-12-17 17:16:50 +0000

    Generate denseSIFT features

--- run3.py
+++ run3.py
@@ -1,8 +1,10 @@
+from sklearn.svm import LinearSVC
 import os
 import time
 import cv2 as cv
 import numpy as np
 import glob
+import matplotlib.pyplot as plt
 
 s_time = time.time()
 class_paths = glob.glob('dataset/training/*')
@@ -34,12 +36,26 @@ def load_test_images(number_of_images):
     return test_images, file_names
 
 
-training_data, training_labels = load_train_images(100)
-testing_data, testing_filenames = load_test_images(100)
-print(testing_filenames)
+# Function to generate denseSIFT features for an image
+def gen_denseSIFT_features(img):
+    step_size = 3
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    sift = cv.xfeatures2d.SIFT_create()
+    # kp is a list of keypoints
+    kp = [cv.KeyPoint(x, y, step_size) for y in range(0, gray.shape[0], step_size)
+          for x in range(0, gray.shape[1], step_size)]
+    # desc is the denseSIFT descriptors
+    desc = sift.compute(gray, kp)
+
+    img = cv.drawKeypoints(gray, kp, img)
+    plt.imshow(cv.drawKeypoints(gray, kp, img))
+    plt.show()
+    return kp, desc
 
 
+# training_data, training_labels = load_train_images(100)
+# testing_data, testing_filenames = load_test_images(100)
+bedroom = cv.imread('dataset/training/bedroom/0.jpg')
+gen_denseSIFT_features(bedroom)
 e_time = time.time()
 print("Processing " + str(e_time - s_time) + 's')
-
-
