a5f800449c28af5a6338b8d469fb819b7d0eb414
Chenfan Wu <cw1a20@soton.ac.uk>
2022-12-26 10:16:04 +0000

    Build the codebook

--- run3.py
+++ run3.py
@@ -1,10 +1,10 @@
-from sklearn.svm import LinearSVC
 import os
 import time
 import cv2 as cv
 import numpy as np
 import glob
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 s_time = time.time()
 class_paths = glob.glob('dataset/training/*')
@@ -29,19 +29,20 @@ def load_test_images(number_of_images):
     test_images = []
     file_names = []
     image_paths = glob.glob('dataset/testing/*')
-    image_paths = image_paths[:100]
+    image_paths = image_paths[:number_of_images]
     for image_path in image_paths:
         test_images.append(cv.imread(image_path, 0))
         file_names.append(image_path[16:])
     return test_images, file_names
 
 
-# Function to generate denseSIFT features for an image
+# Function to generate denseSIFT features for an image.
+# Parameter: img is the path of an image file
 def gen_denseSIFT_features(img):
     step_size = 3
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     sift = cv.xfeatures2d.SIFT_create()
-    # kp is a list of keypoints
+    # kp is a list of keypoints obtained by scanning pixels
     kp = [cv.KeyPoint(x, y, step_size) for y in range(0, gray.shape[0], step_size)
           for x in range(0, gray.shape[1], step_size)]
     # desc is the denseSIFT descriptors
@@ -50,12 +51,24 @@ def gen_denseSIFT_features(img):
     img = cv.drawKeypoints(gray, kp, img)
     plt.imshow(cv.drawKeypoints(gray, kp, img))
     plt.show()
-    return kp, desc
+    return desc
+
+
+# Build the codebook
+# desc is the features we generate from training images; k is the number of clusters
+def gen_codebook(desc, k):
+    kmeans = KMeans(n_clusters=k, random_state=0)
+    kmeans.fit(desc)
+    codebook = kmeans.cluster_centers_.squeeze()
+    print("Successfully build codebook")
+    return codebook
 
 
 # training_data, training_labels = load_train_images(100)
 # testing_data, testing_filenames = load_test_images(100)
 bedroom = cv.imread('dataset/training/bedroom/0.jpg')
-gen_denseSIFT_features(bedroom)
+print(gen_denseSIFT_features(bedroom))
 e_time = time.time()
 print("Processing " + str(e_time - s_time) + 's')
+
+
