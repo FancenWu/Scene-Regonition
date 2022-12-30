8f5289bdc4672a15291bca11cda2c31f7ecb33f2
Chenfan Wu <cw1a20@soton.ac.uk>
2022-12-30 20:23:20 +0000

    Building codebook takes too much time

--- run3.py
+++ run3.py
@@ -22,6 +22,7 @@ def load_train_images(num_per_class):
         for image_path in image_paths:
             train_images.append(cv.imread(image_path, 0))
             train_labels.append(class_name)
+    print("Success to load train images")
     return train_images, train_labels
 
 
@@ -34,11 +35,12 @@ def load_test_images(number_of_images):
     for image_path in image_paths:
         test_images.append(cv.imread(image_path, 0))
         file_names.append(image_path[16:])
+    print("Success to load test images")
     return test_images, file_names
 
 
 # Function to generate denseSIFT features for an image.
-# Parameter: img is the path of an image file
+# Parameter: data is a set of image files
 def gen_denseSIFT_features(img):
     step_size = 2
     # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -47,31 +49,16 @@ def gen_denseSIFT_features(img):
     kp = [cv.KeyPoint(x, y, step_size) for y in range(0, img.shape[0], step_size)
           for x in range(0, img.shape[1], step_size)]
     # desc is the denseSIFT descriptors
-    desc = sift.compute(img, kp)
-
-    # img = cv.drawKeypoints(gray, kp, img)
-    # plt.imshow(cv.drawKeypoints(gray, kp, img))
-    # plt.show()
+    desc = sift.compute(img, kp)[1]
     return desc
 
 
-# Return all descriptors from a set of images
-def get_all_descriptors(data):
-    desc_list = []
-    for i in range(len(data)):
-        desc = gen_denseSIFT_features(data[i])
-        desc_list.append(desc)
-    result = np.asarray(desc_list)
-    return result
-
-
 # Build the codebook
-# descs is set of descriptors from images; k is the number of clusters
+# desc_list is set of descriptors; k is the number of clusters
 def gen_codebook(desc_list, k):
-    features = np.vstack((desc for desc in desc_list))
-    kmeans = KMeans(n_clusters=k, random_state=0)
-    kmeans.fit(features)
-    codebook = kmeans.cluster_centers_.squeeze()
+    features = np.vstack((descriptor for descriptor in desc_list))
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(features)
+    codebook = kmeans.cluster_centers_
     print("Successfully build codebook")
     return codebook
 
@@ -87,7 +74,7 @@ def build_spatial_pyramid(img, level, codebook, k):
         if l == 0:
             desc0 = gen_denseSIFT_features(img)
             predict0 = codebook.predict(desc0)
-            hist0 = np.bincount(predict0, minlength=k).ravel()
+            hist0 = np.bincount(predict0, minlength=k).reshape(1, -1).ravel()
             pyramid.append(hist0*0.25)
         if l == 1:
             a, b = 0, 0
@@ -96,7 +83,7 @@ def build_spatial_pyramid(img, level, codebook, k):
                 for x in range(1, 3):
                     desc1 = gen_denseSIFT_features(img[a: a + height//2, b: b + width//2])
                     predict1 = codebook.predict(desc1)
-                    hist1 = np.bincount(predict1, minlength=k).ravel()
+                    hist1 = np.bincount(predict1, minlength=k).reshape(1, -1).ravel()
                     pyramid.append(hist1*0.25)
                     b = b + width//2
                 a = a + height//2
@@ -107,11 +94,12 @@ def build_spatial_pyramid(img, level, codebook, k):
                 for x in range(1, 5):
                     desc2 = gen_denseSIFT_features(img[a: a + height//4, b: b + width//4])
                     predict2 = codebook.predict(desc2)
-                    hist2 = np.bincount(predict2, minlength=k).ravel()
+                    hist2 = np.bincount(predict2, minlength=k).reshape(1, -1).ravel()
                     pyramid.append(hist2*0.5)
                     b = b + width//4
                 a = a + height//4
-    pyramid = np.asarray(pyramid).ravel()
+    pyramid = np.array(pyramid).ravel()
+    print("Success to build the spatial pyramid")
     return pyramid
 
 
@@ -121,14 +109,24 @@ def get_pyramid(data, level, codebook, k):
     for i in range(len(data)):
         pyramid = build_spatial_pyramid(data[i], level, codebook, k)
         result.append(pyramid)
-        result = np.asarray(result)
+        result = np.array(result)
         return result
 
 
 training_data, training_labels = load_train_images(100)
 testing_data, testing_filenames = load_test_images(100)
-k = 200
-codebook = gen_codebook(get_all_descriptors(training_data), k)
+k = 100
+train_desc = [gen_denseSIFT_features(img) for img in training_data]
+print("Codebook size: ", k)
+print("Building the Codebook, it will take some time")
+all_train_desc = []
+for i in range(len(train_desc)):
+    for j in range(train_desc[i].shape[0]):
+        all_train_desc.append(train_desc[i][j, :])
+
+all_train_desc = np.array(all_train_desc)
+codebook = gen_codebook(all_train_desc, k)
+
 training_hist = get_pyramid(training_data, 2, codebook, k)
 testing_hist = get_pyramid(testing_data, 2, codebook, k)
 
@@ -137,8 +135,7 @@ testing_hist = get_pyramid(testing_data, 2, codebook, k)
 clf = LinearSVC(random_state=0)
 clf.fit(training_hist, training_labels)
 predict = clf.predict(testing_hist)
-
-
+print(predict)
 e_time = time.time()
 print("Processing " + str(e_time - s_time) + 's')
 
